73b8a62f8377641b807cde7fa2babeb645d23704
Marko Mijakovski <m.mijakovski07@gmail.com>
2025-03-19 18:57:00 +0100

    I've made it responsive

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base:"/web-pages",
+    base: '/convert-to-binary/',
     resolve: {
         extensions: ['.js', '.jsx', '.json', '.avif'],
     },  
